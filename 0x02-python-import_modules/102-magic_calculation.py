2d2163096389768bc5034252133ed228fc707d0e
belloakinwunmi <belloakinwunmi@gmail.com>
2022-12-01 06:10:14 -0800

    task

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,8 +3,8 @@
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if (a < b):
-        c = add(a, b)
-        for z in range(4, 6):
-	c = add(c, z)
-        return(c)
+    c = add(a, b)
+    for z in range(4, 6):
+    c = add(c, z)
+    return(c)
     return(sub(a, b))
