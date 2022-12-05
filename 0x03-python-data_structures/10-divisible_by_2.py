efb8bc9146777c89f2620b68fc8dc955e54790fd
belloakinwunmi <belloakinwunmi@gmail.com>
2022-12-05 02:29:12 -0800

    task10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 def divisible_by_2(my_list=[]):
     new = []
     for x in my_list:
-        if x % 2 is 0:
-            new.append(True)
-        else:
-            new.append(False)
+    if x % 2 is 0:
+        new.append(True)
+    else:
+        new.append(False)
     return new
